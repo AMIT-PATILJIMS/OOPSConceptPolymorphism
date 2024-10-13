c617333d6c00614da58bd800debfb626f2d636bd
Amit Patil <ampatilofficial49@gmail.com>
2024-10-13 15:54:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace OOPSConceptPolymorphism
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape obj = new Triangle();
+
+            obj.Draw();
+            /*
+             * It will print
+             * Performing base class drawing tasks
+             */
+
+            /*
+             * Reason : In the derived class we are using "new" keyword
+             */
+
+        }
+    }
+
+    public class Shape
+    {
+        // A few example members
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        // Virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+
+    public class Triangle : Shape
+    {
+        public new void Draw()
+        {
+            // Code to draw a triangle...
+            Console.WriteLine("Drawing a triangle");
+            //base.Draw();
+        }
+    }
+}
